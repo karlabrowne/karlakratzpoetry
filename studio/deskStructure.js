2e6b7a10046b34418d2f669a1ad556de7de02e7a
andypbrowne <andypbrowne@gmail.com>
2021-01-24 13:52:48 -0500

    remove poem link from body portable text and re-orderitem s in the desk structure

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,6 +1,7 @@
 import S from '@sanity/desk-tool/structure-builder'
 import MdSettings from 'react-icons/lib/md/settings'
 import MdPerson from 'react-icons/lib/md/person'
+import FiFeather from 'react-icons/fi'
 
 const hiddenDocTypes = listItem =>
   !['category', 'author', 'post', 'siteSettings'].includes(listItem.getId())
@@ -9,6 +10,21 @@ export default () =>
   S.list()
     .title('Content')
     .items([
+      S.listItem()
+        .title('Poems')
+        .icon(FiFeather)
+        .schemaType('poem')
+        .child(S.documentTypeList('poem').title('Poem')),
+      S.listItem()
+        .title('Authors')
+        .icon(MdPerson)
+        .schemaType('author')
+        .child(S.documentTypeList('author').title('Authors')),
+      S.listItem()
+        .title('Categories')
+        .schemaType('category')
+        .child(S.documentTypeList('category').title('Categories')),
+      S.divider(),
       S.listItem()
         .title('Settings')
         .icon(MdSettings)
@@ -22,15 +38,6 @@ export default () =>
         .title('Blog posts')
         .schemaType('post')
         .child(S.documentTypeList('post').title('Blog posts')),
-      S.listItem()
-        .title('Authors')
-        .icon(MdPerson)
-        .schemaType('author')
-        .child(S.documentTypeList('author').title('Authors')),
-      S.listItem()
-        .title('Categories')
-        .schemaType('category')
-        .child(S.documentTypeList('category').title('Categories')),
       // This returns an array of all the document types
       // defined in schema.js. We filter out those that we have
       // defined the structure above
