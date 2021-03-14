e3e8578865b496acd867308a82931ab66595ddcf
andypbrowne <andypbrowne@gmail.com>
2021-03-13 23:12:09 -0500

    add blog schema back to desk structure to allow deletion of two unused categories

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -48,11 +48,11 @@ export default () =>
 		S.listItem()
 			.title('Settings')
 			.icon(MdSettings)
-			.child(S.editor().id('siteSettings').schemaType('siteSettings').documentId('siteSettings'))
-		// S.listItem()
-			// .title('Blog posts')
-			// .schemaType('post')
-			// .child(S.documentTypeList('post').title('Blog posts')),
+			.child(S.editor().id('siteSettings').schemaType('siteSettings').documentId('siteSettings')),
+		S.listItem()
+			.title('Blog posts')
+			.schemaType('post')
+			.child(S.documentTypeList('post').title('Blog posts'))
 		// This returns an array of all the document types
 		// defined in schema.js. We filter out those that we have
 		// defined the structure above
