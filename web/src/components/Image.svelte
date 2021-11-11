e518cb04a1425a170e6c5c3fdbb323b069296314
Derek Nguyen <derek@penandpillow.com>
2021-11-11 16:29:50 +0900

    feat: smooth image transition, reduce file size

---
+++ web/src/components/Image.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+
+  export let src: string;
+  export let alt: string;
+  export let srcset: string;
+  let loaded = false;
+
+  const onload = () => {
+    loaded = true
+  };
+</script>
+
+{#if loaded}
+  <img {src} {srcset} {alt} transition:fade />
+{:else}
+  <img aria-hidden class="temp_img" {src} {srcset} {alt} on:load={onload} />
+{/if}
+
+<style>
+  .temp_img {
+    visibility: hidden;
+  }
+</style>
