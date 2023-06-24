8904c08349da0b269fdcefced49bfca276e9f626
lolusername <to.atilio@gmail.com>
2023-06-23 21:50:15 -0400

    sanity v3

--- studio/schemas/documents/poem.js
+++ studio/schemas/documents/poem.js
@@ -1,35 +1,35 @@
-import {FiFeather} from 'react-icons/fi'
+// import {FiFeather} from 'react-icons/fi'
 
 export default {
   name: 'poem',
   type: 'document',
   title: 'Poems',
-  icon: FiFeather,
+  //   icon: FiFeather,
   fields: [
     {
       name: 'name',
       type: 'string',
       title: 'Poem Title',
-      validation: Rule => Rule.required()
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'slug',
       type: 'slug',
       title: 'Slug',
-      validation: Rule => Rule.required(),
+      validation: (Rule) => Rule.required(),
       description: 'Some frontends will require a slug to be set to be able to show the person',
       options: {
         source: 'name',
-        maxLength: 600
-      }
+        maxLength: 600,
+      },
     },
     {
       name: 'poemImage',
       type: 'mainImage',
       title: 'Poem image',
       options: {
-        hotspot: true
-      }
+        hotspot: true,
+      },
     },
     {
       name: 'dateWritten',
@@ -38,8 +38,8 @@ export default {
       description: 'This is a reminder to the author of the original date the poem was written',
       options: {
         dateFormat: 'YYYY-MM-DD',
-        calendarTodayLabel: 'Today'
-      }
+        calendarTodayLabel: 'Today',
+      },
     },
     {
       name: 'dateEdited',
@@ -48,8 +48,8 @@ export default {
       description: 'This is a reminder to the author of the last time the poem itself was edited',
       options: {
         dateFormat: 'YYYY-MM-DD',
-        calendarTodayLabel: 'Today'
-      }
+        calendarTodayLabel: 'Today',
+      },
     },
     {
       name: 'authors',
@@ -57,63 +57,64 @@ export default {
       type: 'array',
       of: [
         {
-          type: 'authorReference'
-        }
-      ]
+          type: 'authorReference',
+        },
+      ],
     },
     {
       name: 'categories',
       type: 'array',
       title: 'Categories',
-      validation: Rule => Rule.required(),
+      validation: (Rule) => Rule.required(),
       of: [
         {
           type: 'reference',
           to: {
-            type: 'category'
-          }
-        }
-      ]
+            type: 'category',
+          },
+        },
+      ],
     },
     {
       title: 'Feature this poem',
-      description: 'Toggling this field to true will make this poem more likely to show up at the top of lists and the most recent featured poem will be linked from the homepage',
+      description:
+        'Toggling this field to true will make this poem more likely to show up at the top of lists and the most recent featured poem will be linked from the homepage',
       name: 'featured',
-      type: 'boolean'
-
+      type: 'boolean',
     },
     {
       name: 'content',
       type: 'bodyPortableText',
-      title: 'Poem content'
+      title: 'Poem content',
     },
     {
       name: 'backgroundTitle',
       type: 'string',
       title: 'Background Title',
-      description: 'This heading can be different for every poem. It is only required if you also have a poem background written'
+      description:
+        'This heading can be different for every poem. It is only required if you also have a poem background written',
     },
     {
       name: 'background',
       type: 'bodyPortableText',
-      title: 'Poem Background'
-    }
+      title: 'Poem Background',
+    },
   ],
   preview: {
     select: {
       title: 'name',
       media: 'poemImage',
       subtitle: 'dateEdited',
-      featured: 'featured'
+      featured: 'featured',
     },
-    prepare ({title = 'No title', media, subtitle, featured}) {
+    prepare({title = 'No title', media, subtitle, featured}) {
       const isFeatured = featured ? `🟢  Featured | ` : ''
       const lastRevised = `Last revised: ${subtitle || 'unknown'}`
       return {
         title,
         media,
-        subtitle: `${isFeatured}${lastRevised}`
+        subtitle: `${isFeatured}${lastRevised}`,
       }
-    }
-  }
+    },
+  },
 }
